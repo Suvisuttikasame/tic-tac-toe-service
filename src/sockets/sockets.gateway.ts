978429c90e1d8e45bfdd5f4de92c5f821e61c0c2
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-03-17 19:04:18 +0700

    - [chore] delete unused files

--- src/sockets/sockets.gateway.ts
+++ src/sockets/sockets.gateway.ts
@@ -4,35 +4,14 @@ import {
   MessageBody,
 } from '@nestjs/websockets';
 import { SocketsService } from './sockets.service';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
 
-@WebSocketGateway()
+@WebSocketGateway(8080)
 export class SocketsGateway {
   constructor(private readonly socketsService: SocketsService) {}
 
-  @SubscribeMessage('createSocket')
-  create(@MessageBody() createSocketDto: CreateSocketDto) {
-    return this.socketsService.create(createSocketDto);
-  }
-
-  @SubscribeMessage('findAllSockets')
-  findAll() {
-    return this.socketsService.findAll();
-  }
-
-  @SubscribeMessage('findOneSocket')
-  findOne(@MessageBody() id: number) {
-    return this.socketsService.findOne(id);
-  }
-
-  @SubscribeMessage('updateSocket')
-  update(@MessageBody() updateSocketDto: UpdateSocketDto) {
-    return this.socketsService.update(updateSocketDto.id, updateSocketDto);
-  }
-
-  @SubscribeMessage('removeSocket')
-  remove(@MessageBody() id: number) {
-    return this.socketsService.remove(id);
+  @SubscribeMessage('create-room')
+  createRoom(@MessageBody() data: string) {
+    console.log('getdata', data);
+    return null;
   }
 }
