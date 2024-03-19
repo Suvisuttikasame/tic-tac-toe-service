6b71d7d42c0f5a385d728bb78794f1c77871a49f
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-03-20 01:19:14 +0700

    - [fix] change to socketio

--- src/sockets/sockets.gateway.ts
+++ src/sockets/sockets.gateway.ts
@@ -2,17 +2,26 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { SocketsService } from './sockets.service';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(8080)
 export class SocketsGateway {
-  constructor(private readonly socketsService: SocketsService) {}
+  // constructor(private readonly socketsService: SocketsService) {}
+  @WebSocketServer()
+  server: Server;
 
   @SubscribeMessage('create-room')
-  async createRoom(@MessageBody() data: string) {
-    const save = await this.socketsService.createRoom(data, 'test');
-    console.log('save', save);
+  async createRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: string,
+  ) {
+    // const save = await this.socketsService.createRoom(data, 'test');
+    // console.log('save', save);
+    console.log('client', client.id);
+    console.log('data', data);
     return null;
   }
 }
